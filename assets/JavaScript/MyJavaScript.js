8691df9638655e2c006787ac068287cdbf6040a4
sadeq <sadeq.yaqobi9219@gmail.com>
2023-05-12 19:40:24 +0330

    modified html , css and js codes for improve readability and understandability by added comment

--- assets/JavaScript/MyJavaScript.js
+++ assets/JavaScript/MyJavaScript.js
@@ -1,66 +1,78 @@
+// Game elements
 const gameMessage = document.querySelector(".game-footer-text");
 const gameBtn = document.querySelector(".game-btn");
 const numberBox = document.querySelector(".numb-box");
 const resetBtn = document.querySelector(".reset-btn");
+
+// Game variables
 let gamePlay = false;
 let score = 0;
+
+// Event listeners
 gameBtn.addEventListener("click", gameBtnFunc);
 resetBtn.addEventListener("click", function () {
   location.reload();
 });
 
+// Function to handle game button click
 function gameBtnFunc() {
   if (!gamePlay) {
+    // Start a new game
     gamePlay = true;
     numberBox.innerHTML = "";
     gameMessage.innerHTML = "";
-    gameBtn.innerHTML = "check";
+    gameBtn.innerHTML = "Check";
     score = 0;
-    maker(6);
+    maker(6); // Create number inputs
   } else {
+    // Check the guesses
     const numbers = document.querySelectorAll(".numb");
     score++;
-    gameMessage.innerHTML = "you'r Guesses : " + score;
+    gameMessage.innerHTML = "Your Guesses: " + score;
     resetBtn.className = "reset-btn";
     let winCondition = 0;
     for (i = 0; i < numbers.length; i++) {
       if (numbers[i].value == numbers[i].correct) {
-        numbers[i].style.backgroundColor = "#59cd90";
+        // Guessed correctly
+        numbers[i].style.backgroundColor = "#59cd90"; // Green
         numbers[i].style.color = "#ffffff";
         winCondition++;
       } else {
-        let color =
-          numbers[i].value < numbers[i].correct ? "#70d6ff" : "#b80c09";
+        // Guessed incorrectly
+        let color = numbers[i].value < numbers[i].correct ? "#70d6ff" : "#b80c09"; // Blue or red
         numbers[i].style.backgroundColor = color;
         numbers[i].style.color = "#ffffff";
       }
       if (winCondition == numbers.length) {
-        gameEnd();
+        gameEnd(); // All numbers guessed correctly, end the game
       }
     }
   }
 }
-function resetGame() {}
+
+// Function to reset the game
+function resetGame() {
+  // TODO: Implement resetGame logic
+}
+
+// Function to end the game
 function gameEnd() {
   if (score <= 6) {
     gameMessage.innerHTML =
-      "you solved the combo with " +
-      "<span class='score-win '>" +
+      "You solved the combo with <span class='score-win'>" +
       score +
-      "</span>" +
-      " guesses <i class='far fa-hand-peace'></i>";
+      "</span> guesses <i class='far fa-hand-peace'></i>";
   } else {
     gameMessage.innerHTML =
-      "you solved the combo with " +
-      "<span class='score-lose '>" +
+      "You solved the combo with <span class='score-lose'>" +
       score +
-      "</span>" +
-      " guesses <i class='far fa-frown'></i>";
+      "</span> guesses <i class='far fa-frown'></i>";
   }
   gameBtn.innerHTML = "Reset";
   gamePlay = false;
 }
 
+// Function to create number inputs
 function maker(num) {
   for (let x = 0; x < num; x++) {
     var el = document.createElement("input");
@@ -75,12 +87,13 @@ function maker(num) {
     el.className = "numb form-control form-control-lg d-inline mx-1";
     numberBox.appendChild(el);
   }
-  // description
+
+  // Description
   let description;
   description = document.createElement("small");
   description.innerHTML =
-    "<b>Blue: </b>choose higher number, <b>red: </b>choose lower number";
+    "<b>Blue: </b>choose a higher number, <b>Red: </b>choose a lower number";
   description.className = "form-text text-muted d-block text-capitalize";
   numberBox.appendChild(description);
-  // end description
+  // End Description
 }
